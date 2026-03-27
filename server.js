1d37b124f8b958876b24047d8eaf037d7d4cc44f
Juan Dela Cruz <neiljunmaglunsod@gmail.com>
2026-03-27 21:37:54 +0800

    add app.listen

--- server.js
+++ server.js
@@ -47,4 +47,8 @@ if (!fs.existsSync('uploads')) {
 // Route
 app.get('/', (req, res) => {
   res.send('Server is running');
+});const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
